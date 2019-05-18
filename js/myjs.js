2f66894892b633fe5b15f4148162e5cb60eb0cd1
g-kakareko <gk15b@my.fsu.edu>
2019-05-18 14:34:09 -0400

    personal-page

---
+++ js/myjs.js
@@ -0,0 +1,16 @@
+$(document).ready(function () 
+{
+    $("#sidebar").mCustomScrollbar(
+    {
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () 
+    {
+        $('#sidebar, #content').toggleClass('active');
+		$(this).toggleClass('active');
+
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
